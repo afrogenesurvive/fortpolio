92cc08a6f67aa664fa97ee6c3bcb5aae577911f3
Michael Grandison <african.genetic.survival@gmail.com>
2020-08-22 22:01:27 -0500

    app js text mod2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactions
+          Learn Reaction Times
         </a>
       </header>
     </div>
